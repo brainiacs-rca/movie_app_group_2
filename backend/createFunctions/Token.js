1e57833f13137953a2ce5e95ab345d8992801674
NIYONSENGA VALENS <igihozocolombe2003@gmail.com>
2022-06-11 21:35:16 +0200

    user:authentication

---
+++ backend/createFunctions/Token.js
@@ -0,0 +1,12 @@
+const jwt = require("jsonwebtoken");
+exports.createToken = async (payload, mode) => {
+  let token;
+  if(mode === "register"){
+    token = jwt.sign({ ...payload }, process.env.SECRET, {
+      expiresIn: "30d",
+    });
+  }else if(mode === "test"){
+    token = jwt.sign({...payload}, process.env.SECRET, {expiresIn: 300000});
+  }
+  return token;
+};
