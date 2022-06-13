bcbda91fa9d59fd13d8e8ad976ac4e7536f5086d
umutonimireille <mireilleumutoni05@gmail.com>
2022-06-13 13:33:04 +0200

    playing videos

--- movie-app/src/components/movie.jsx
+++ movie-app/src/components/movie.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React , {useState}from "react";
 import "./movie.css";
+import movieTrailer from "movie-trailer";
+import Youtube from 'react-youtube';
 import {IoIosAddCircle} from 'react-icons/io';
 const img_api = "https://image.tmdb.org/t/p/w1280";
-function Movie({ title, poster_path, overview, vote_average }) {
+function Movie({ title, poster_path, overview, vote_average,movie }) {
+
+
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl,setTrailerUrl] = useState("");
+
+  const opts = {
+  height:'390',
+  width:'100%',
+  playerVars:{
+    
+    autoplay:1,
+ },
+
+};
+
+  const handleClick = (movie) =>{
+    console.log("Movie function entered",movie)
+    if(trailerUrl){
+      setTrailerUrl('');
+    }else{
+      movieTrailer(movie?.title || "")
+      .then((url) =>{
+        const urlParams = new URLSearchParams( new URL(url).search );
+        setTrailerUrl(urlParams.get("v"));
+      })
+      .catch((error) => console.log(error));
+    }
+
+  };
   return (
     <div>
     <div className="both">
      
     <div className="image">
     
-      <img src={img_api + poster_path} alt={title} />
+      <img src={img_api + poster_path} alt={title} onClick={()=>{handleClick(movie)}}  />
       
      <div className="bottom">
        <div className="btm1">{title}</div>
@@ -22,6 +53,9 @@ function Movie({ title, poster_path, overview, vote_average }) {
      </div>
     </div>
     </div>
+    
+     {trailerUrl && <Youtube  videoId={trailerUrl} opts={opts}/>
+}
    
     </div>
   );
